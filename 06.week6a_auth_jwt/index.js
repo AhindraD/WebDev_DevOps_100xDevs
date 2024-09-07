6c4f84eb3e5bf038508dffcf2f7b114c8466791d
Ahindra D <official.ahindra@gmail.com>
2024-09-07 21:28:43 +0530

    token_based

--- 06.week6a_auth_jwt/index.js
+++ 06.week6a_auth_jwt/index.js
@@ -1,24 +1,31 @@
 const express = require("express")
+const { v4: uuidv4 } = require('uuid');
 
 const app = express()
 app.use(express.json())
 
-const USER_DB = [];
-
-app.post("signup", (req, res) => {
-    const [username, password] = req.body;
-    let currUser = {
-        username,
-        password
+const USER_DB = [
+    {
+        "username": "Jacky.Marks",
+        "password": "y7k8MJSh0fd9Hyk"
     }
+];
 
+app.post("/signup", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    let currUser = {
+        "username": username,
+        "password": password
+    }
+    console.log('currUser', currUser)
     USER_DB.push(currUser);
 
     res.json({ status: "200", data: currUser })
 })
 
 app.post("/signin", (req, res) => {
-    const [username, password] = req.body;
+    const { username, password } = req.body;
     if (!username || !password) { res.json({ status: "400" }) }
 
     const foundUser = USER_DB.find((elem, indx) => {
@@ -32,7 +39,7 @@ app.post("/signin", (req, res) => {
     if (foundUser) {
         console.log("user found")
         const token = uuidv4();
-        res.json({ status: "200", data: token })
+        res.json({ status: "200", token: token })
     }
     else {
         res.json({ status: "400", messsage: "Invalid credentials" })
